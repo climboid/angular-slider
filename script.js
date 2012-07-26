73c543129656dd9ae8ee4e347454e8608c1ca408
Oscar Villarreal <Oscar_Villarreal-FIR@mckinsey.com>
2012-07-25 21:48:14 -0400

    removing unnecesary files

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 angular.module('myApp', [])
-    .directive('slider', function() {
+    .directive('draggable', function() {
     return {
         restrict:'A',
         link:function(scope,element,attrs){
-           element.slider({
-                range: true,
-                min: 0,
-                max: 500,
-                values: [ 75, 300 ],
-                slide: function( event, ui ) {
-
-                }
-            });
+           element.draggable({});
         }
     };
 });
