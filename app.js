1e72a0f2c07a09e8c21fb06ceeb44ea3cb668aac
Oscar Villarreal <Oscar_Villarreal-FIR@mckinsey.com>
2012-07-26 09:24:14 -0400

    removing router

--- app.js
+++
@@ -1,6 +0,0 @@
-angular.module('application',[]).
-  config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-      when('/', {templateUrl: 'templates/slider.html',   controller:PickerCtrl}).
-      otherwise({redirectTo: '/'});
-}]);
