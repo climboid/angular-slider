7996cc3284cec5bac983264bd22ee5971f9ba694
Oscar <oscar_villarreal-FIR@mckinsey.com>
2012-11-01 17:25:00 -0400

    adding multiple sliders

---
+++ controller.js
@@ -0,0 +1,29 @@
+function mainCtrl($scope){
+
+	$scope.hello = "hello"; //first input text
+	$scope.goodbye = "goodbye"; //second input text
+}
+
+angular.module('myApp', [])
+    .directive('slider', function() {
+    return {
+        restrict:'A',
+        link:function(scope,element,attrs){
+           element.slider({
+                orientation: "vertical",
+                min: 0,
+                max: 500,
+                value: 75,
+                slide: function( event, ui ) {
+                		if(event.target.id =="slider1"){
+                			scope.hello = ui.value;	
+                		}else if(event.target.id == "slider2"){
+                			scope.goodbye = ui.value;
+                		}
+                    
+                    scope.$apply();
+                }
+            });
+        }
+    };
+});
