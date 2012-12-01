45e35f2ebd910e4d579b744e033165e7f8317d0a
Oscar <oscar_villarreal-FIR@mckinsey.com>
2012-12-01 12:00:53 -0500

    updates

--- controller.js
+++ controller.js
@@ -1,7 +1,6 @@
 function mainCtrl($scope){
 
 	$scope.hello = "hello"; //first input text
-	$scope.goodbye = "goodbye"; //second input text
 
 }
 
@@ -11,18 +10,26 @@ angular.module('myApp', [])
         restrict:'A',
         link:function(scope,element,attrs){
            element.slider({
-                min: 0,
-                max: 500,
-                value: 75,
+                min: 1,
+                max: 3,
+                value: 2,
                 slide: function( event, ui ) {
-                		if(event.target.id =="slider1"){
-                			scope.hello = ui.value;	
-                		}else if(event.target.id == "slider2"){
-                			scope.goodbye = ui.value;
-                		}
-                    
-                    scope.$apply();
-                }
+                    if(ui.value == 3){
+                        $(this).find('a').css('margin-left','-31%')
+                    }else if(ui.value == 2){
+                        $(this).find('a').css('margin-left','-15%')
+                    }else if(ui.value == 1){
+                        $(this).find('a').css('margin-left','0%')
+                    }else{
+                        $(this).find('a').css('margin-left','-15%')
+                    }
+            		if(event.target.id =="slider1"){
+            			scope.hello = ui.value;	
+            		}else if(event.target.id == "slider2"){
+            			scope.goodbye = ui.value;
+            		}
+                scope.$apply();
+            }
             });
         }
     };
